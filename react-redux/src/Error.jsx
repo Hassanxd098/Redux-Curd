4ed646effe37b547b5bcec55308c613ecaca7b84
Hassanxd098 <pmhassanurrahman@gmail.com>
2025-09-18 16:48:16 +0530

    bkws

--- react-redux/src/Error.jsx
+++ react-redux/src/Error.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Error() {
   return (
     <div>
-       <div className=' w-[100%]  h-[70vh]  flex flex-col items-center justify-center'>
-        <div className=' text-red-700 text-3xl'>
-            <h1>404! Error</h1>
+      <div className=" w-[100%]  h-[70vh]  flex flex-col items-center justify-center">
+        <div className=" text-red-700 text-3xl">
+          <h1>404! Error chaljaaaaaaaa</h1>
         </div>
-        <div className='w-[50%] font-bold'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, fuga beatae explicabo, itaque odio dolorem quibusdam voluptatum exercitationem quia vitae ullam delectus nam minus omnis officia rem necessitatibus nihil tempore?
+        <div className="w-[50%] font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+          fuga beatae explicabo, itaque odio dolorem quibusdam voluptatum
+          exercitationem quia vitae ullam delectus nam minus omnis officia rem
+          necessitatibus nihil tempore?
         </div>
-       </div>
+      </div>
     </div>
-  )
+  );
 }
